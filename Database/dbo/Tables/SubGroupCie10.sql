c1387377e364506a9e97ab00714edfac6a3120ad
Leandro Baena Torres <leandrobaena@paradigma.com.co>
2023-10-30 18:05:11 -0500

    Reestructuración campos CIE10

--- Database/dbo/Tables/SubGroupCie10.sql
+++ Database/dbo/Tables/SubGroupCie10.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[SubGroupCie10]
 (
-	[SubGroupCie10Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+	[SubGroupCie10Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [Code] CHAR(3) NOT NULL,
     [Name] VARCHAR(200) NOT NULL, 
     [Causes] VARCHAR(200) NOT NULL, 
     [GroupCie10Id] INT NOT NULL, 
