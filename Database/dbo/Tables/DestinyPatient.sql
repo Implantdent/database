ae3963aba18c546b385155e9d6270de2a3548d21
Leandro Baena Torres <leandrobaena@paradigma.com.co>
2024-06-28 11:07:09 -0500

    Adición de tabla DestinyPatient

---
+++ Database/dbo/Tables/DestinyPatient.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[DestinyPatient]
+(
+	[DestinyPatientId] SMALLINT NOT NULL PRIMARY KEY IDENTITY, 
+    [Code] CHAR(2) NOT NULL, 
+    [Name] VARCHAR(100) NOT NULL
+)
+
+GO
+
+CREATE UNIQUE INDEX [UK_DestinyPatient_Code] ON [dbo].[DestinyPatient] ([Code])
