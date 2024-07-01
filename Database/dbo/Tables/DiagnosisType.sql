395f3ad54521b5b6afda486dc312420fdb1df0b4
Leandro Baena Torres <leandrobaena@paradigma.com.co>
2024-07-01 12:09:41 -0500

    Ajuste en tablas

---
+++ Database/dbo/Tables/DiagnosisType.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[DiagnosisType]
+(
+	[DiagnosisTypeId] SMALLINT NOT NULL PRIMARY KEY IDENTITY, 
+    [Code] CHAR(2) NOT NULL, 
+    [Name] VARCHAR(50) NOT NULL
+)
+
+GO
+
+CREATE UNIQUE INDEX [UK_DiagnosisType_Code] ON [dbo].[DiagnosisType] ([Code])
