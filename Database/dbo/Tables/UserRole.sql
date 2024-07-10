4de6b5ecad34f7d8de4ec91281aded261cd5f5c5
Leandro Baena Torres <leandrobaena@paradigma.com.co>
2024-07-10 09:40:11 -0500

    Adición de tablas para roles

---
+++ Database/dbo/Tables/UserRole.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[UserRole]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY,
+    [UserId] SMALLINT NOT NULL,
+    [RoleId] SMALLINT NOT NULL,
+    CONSTRAINT [FK_UserRole_User] FOREIGN KEY ([UserId]) REFERENCES [User]([UserId]),
+    CONSTRAINT [FK_UserRole_Role] FOREIGN KEY ([RoleId]) REFERENCES [Role]([RoleId])
+)
+
+GO
+
+CREATE UNIQUE INDEX [UK_UserRole] ON [dbo].[UserRole] ([UserId], [RoleId])
