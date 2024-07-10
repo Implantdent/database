2005cae445386994b827b202705221cb59f0d5e3
Leandro Baena Torres <leandrobaena@paradigma.com.co>
2024-07-10 11:43:28 -0500

    Adición de tablas de logs

---
+++ Database/dbo/Tables/LogComponent.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[LogComponent]
+(
+	[LogComponentId] BIGINT NOT NULL PRIMARY KEY IDENTITY, 
+    [Date] DATETIME NOT NULL, 
+    [Type] CHAR NOT NULL, 
+    [Controller] VARCHAR(45) NOT NULL, 
+    [Method] VARCHAR(45) NOT NULL, 
+    [Input] TEXT NOT NULL, 
+    [Output] TEXT NOT NULL, 
+    [UserId] SMALLINT NOT NULL, 
+    CONSTRAINT [FK_LogComponent_User] FOREIGN KEY ([UserId]) REFERENCES [User]([UserId])
+)
