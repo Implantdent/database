3d00052c14eaf9d18a0ae4b626c54fe72e6984c3
Leandro Baena Torres <leandrobaena@paradigma.com.co>
2024-07-11 16:16:53 -0500

    Creación vista de usuarios x roles

---
+++ Database/dbo/Views/VwUserRole.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[VwUserRole] AS
+SELECT
+	u.UserId, u.Email, u.Name AS [User], u.Active, r.RoleId, r.Name AS [Role]
+FROM
+	[User] u
+	INNER JOIN [UserRole] ur
+		ON u.UserId = ur.UserId
+	INNER JOIN [Role] r
+		ON ur.RoleId = r.RoleId
